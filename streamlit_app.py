03f8ba6c578ee1d7969f6170aa8e649c0caf3f3f
shubhhammane-55 <shubhham11111@gmail.com>
2026-08-14 11:55:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ cnx=st.connection("snowflake")
 session=cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
+st.stop()
 
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
